e8aa1b777df25696bd6960ab3c4f4787a1561159
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-02-20 02:09:42 -0600

    Add some animation in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          "0%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateY(1)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
+      },
+    },
   },
   variants: {
     extend: {},
