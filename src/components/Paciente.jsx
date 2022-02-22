0b903662a3c5a1bb69a1ea5ff0dc6bb70f75333e
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-02-21 23:32:10 -0600

    Add alert message when want to delete an element
    Add alert using sweetalert in order to confirm is we want to delete a
    register.

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,24 @@
+import swal from "sweetalert";
+
 const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
+  const handleDelete = () => {
+    swal({
+      title: "Estas seguro de eliminar el registro?",
+      icon: "warning",
+      buttons: ["Cancelar", "Si, eliminar"],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        eliminarPaciente(paciente.id);
+        swal("Registro eliminado", {
+          icon: "success",
+        });
+      }
+    });
+  };
+
   return (
-    <div className="m-3 bg-white shadow-xl px-5 py-5 rounded-xl mb-6">
+    <div className="bg-white shadow-xl px-5 py-5 rounded-xl mb-6">
       <p className="font-bold mb-2 text-blue-400 capitalize">
         Nombre:{" "}
         <span className="normal-case font-normal text-black">
@@ -42,7 +60,7 @@ const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
         <button
           type="button"
           className="bg-red-600 hover:bg-red-700 transition-all duration-150  hover:shadow-xl hover:-translate-y-0.5 transform py-1 px-8 text-white capitalize font-bold rounded-lg cursor-pointer"
-          onClick={() => eliminarPaciente(paciente.id)}
+          onClick={handleDelete}
         >
           Eliminar
         </button>
