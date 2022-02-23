1922c144b2a655131fe43a5e4f4ac32b3822a159
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-02-22 18:07:45 -0600

    Integrate localStorage to save data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import Form from "./components/Form";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+  /*-------------------------------------------------- States ------------------------------------------------------*/
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
+  /*-------------------------------------------------- useEffect ------------------------------------------------------*/
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  /*-------------------------------------------------- Helpers functions ------------------------------------------------------*/
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter(
       (paciente) => paciente.id !== id
